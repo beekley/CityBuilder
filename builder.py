89e128643cdb0a15c805e8f2d7965242455c0239
Brett Beekley <brettbeekley@gmail.com>
2013-05-29 16:01:34 +0200

    Update builder.py

--- builder.py
+++ builder.py
@@ -26,29 +26,58 @@ class Plot():
 		self.value = 0
 		self.evaluate()
 		
+	def populate(self, populant):
+		self.occupant = populant
+		self.occupied = True
+		self.evaluate()
+		if self.value > 50:
+			self.UpdateAdjacent()
+		
 	def evaluate(self):
 		sidecount = 0
 		totalvalue = 0
+		initialvalue = self.value
 		
 		try:
-			totalvalue = Grid[self.y+1][self.x].occupant.wealth
+			totalvalue = Grid[self.y+1][self.x].value
 		except IndexError:
 			sidecount += 1
 		try:
-			totalvalue += Grid[self.y][self.x+1].occupant.wealth
+			totalvalue += Grid[self.y][self.x+1].value
 		except IndexError:
 			sidecount += 1
 		try:
-			totalvalue += Grid[self.y-1][self.x].occupant.wealth
+			totalvalue += Grid[self.y-1][self.x].value
 		except IndexError:
 			sidecount += 1
 		try:
-			totalvalue += Grid[self.y][self.x-1].occupant.wealth
+			totalvalue += Grid[self.y][self.x-1].value
 		except IndexError:
 			sidecount += 1
 		
 		if sidecount < 4:
-			self.value = totalvalue / (4-sidecount)
+			self.value = ( 2 * self.occupant.wealth + totalvalue) / (6-sidecount)
+			
+		if self.value > 50 and abs(initialvalue - self.value) > 50:
+			self.UpdateAdjacent()
+			
+	def UpdateAdjacent(self):
+		try:
+			Grid[self.y+1][self.x].evaluate()
+		except IndexError:
+			pass
+		try:
+			Grid[self.y][self.x+1].evaluate()
+		except IndexError:
+			pass
+		try:
+			Grid[self.y-1][self.x].evaluate()
+		except IndexError:
+			pass
+		try:
+			Grid[self.y][self.x-1].evaluate()
+		except IndexError:
+			pass
 		
 def AddCitizens():
 	for i in range(Population):
@@ -71,6 +100,9 @@ def PrintGrid():
 	for i in range(Width):
 		Line = '|'
 		for j in range(Width):
-			Line = Line + str(Grid[i][j].occupant.wealth) + '|'
-		print(str(Width-i) + Line)
+			Line = Line + str(int(Grid[i][j].value)).zfill(4) + '|'
+		print(str(Width-i).zfill(2) + Line)
 	print('\n')
+	
+BuildGrid()
+Guy = Citizen()
